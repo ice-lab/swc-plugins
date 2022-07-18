b81cf6e87b8a49c3c74145e8e6abd2fa567a6e90
水澜 <chenjun_1011@hotmail.com>
2022-07-18 19:46:06 +0800

    feat: remove export (#1)
    * feat: add remove export plugin
    
    * chore: update lock file
    
    * chore: beta version
    
    * tests: remove export
    
    * chore: ci
    
    * chore: rust config
    
    * fix: tool chain version

---
+++ packages/remove-export/tests/fixtrue.rs
@@ -0,0 +1,114 @@
+use std::path::PathBuf;
+use swc_ecma_parser::{EsConfig, Syntax};
+use swc_ecma_transforms_testing::{test, test_fixture};
+use swc_plugin_remove_export::{remove_export_exprs};
+
+#[testing::fixture("tests/fixture/base/input.js")]
+fn fixture_base(input: PathBuf) {
+  let parent = input.parent().unwrap();
+  let output = parent.join("output.js");
+
+  test_fixture(
+    Default::default(),
+    &|_t| {
+      remove_export_exprs([String::from("getData")].to_vec())
+    },
+    &input,
+    &output,
+  );
+}
+
+#[testing::fixture("tests/fixture/preserveConfig/**/input.js")]
+fn fixture_preserve_config(input: PathBuf) {
+  let parent = input.parent().unwrap();
+  let output = parent.join("output.js");
+
+  test_fixture(
+    Syntax::Es(EsConfig {
+      decorators: true,
+      jsx: true,
+      ..Default::default()
+    }),
+    &|_t| {
+      remove_export_exprs([String::from("getData"), String::from("default")].to_vec())
+    },
+    &input,
+    &output,
+  );
+}
+
+#[testing::fixture("tests/fixture/preserveConfigAndDefault/**/input.js")]
+fn fixture_preserve_config_and_config(input: PathBuf) {
+  let parent = input.parent().unwrap();
+  let output = parent.join("output.js");
+
+  test_fixture(
+    Syntax::Es(EsConfig {
+      decorators: true,
+      jsx: true,
+      ..Default::default()
+    }),
+    &|_t| {
+      remove_export_exprs([String::from("getData")].to_vec())
+    },
+    &input,
+    &output,
+  );
+}
+
+#[testing::fixture("tests/fixture/preserveData/**/input.js")]
+fn fixture_preserve_data(input: PathBuf) {
+  let parent = input.parent().unwrap();
+  let output = parent.join("output.js");
+
+  test_fixture(
+    Syntax::Es(EsConfig {
+      decorators: true,
+      jsx: true,
+      ..Default::default()
+    }),
+    &|_t| {
+      remove_export_exprs([String::from("getConfig"), String::from("default")].to_vec())
+    },
+    &input,
+    &output,
+  );
+}
+
+#[testing::fixture("tests/fixture/preserveDefault/**/input.js")]
+fn fixture_preserve_default(input: PathBuf) {
+  let parent = input.parent().unwrap();
+  let output = parent.join("output.js");
+
+  test_fixture(
+    Syntax::Es(EsConfig {
+      decorators: true,
+      jsx: true,
+      ..Default::default()
+    }),
+    &|_t| {
+      remove_export_exprs([String::from("getConfig"), String::from("getData")].to_vec())
+    },
+    &input,
+    &output,
+  );
+}
+
+#[testing::fixture("tests/fixture/removeData/**/input.js")]
+fn fixture_remove_data(input: PathBuf) {
+  let parent = input.parent().unwrap();
+  let output = parent.join("output.js");
+
+  test_fixture(
+    Syntax::Es(EsConfig {
+      decorators: true,
+      jsx: true,
+      ..Default::default()
+    }),
+    &|_t| {
+      remove_export_exprs([String::from("getData")].to_vec())
+    },
+    &input,
+    &output,
+  );
+}
